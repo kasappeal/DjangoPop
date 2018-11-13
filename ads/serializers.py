b4872b2e702e48fa57948c661f2eb3c64b8464a3
Alberto Casero <alberto@kasfactory.net>
2018-11-14 00:23:54 +0100

    Implementamos el API completa de anuncios

---
+++ ads/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from ads.models import Ad
+
+
+class AdListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = Ad
+        fields = ['id', 'name', 'price', 'image']
+
+
+class AdSerializer(AdListSerializer):
+
+    class Meta(AdListSerializer.Meta):
+        fields = '__all__'
--- ads/urls.py
+++ ads/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
+from ads.api import AdListAPIView, AdDetailAPIView
 from ads.views import HomeView, AdDetailView, NewAdView
 
 urlpatterns = [
     path('ads/<int:pk>', AdDetailView.as_view(), name='ad_detail'),
     path('ads/new', NewAdView.as_view(), name='new_ad'),
-    path('', HomeView.as_view(), name='home')
+    path('', HomeView.as_view(), name='home'),
+
+    # API
+    path('api/1.0/ads/', AdListAPIView.as_view(), name='ad_list_api'),
+    path('api/1.0/ads/<int:pk>', AdDetailAPIView.as_view(), name='ad_detail_api')
 ]
