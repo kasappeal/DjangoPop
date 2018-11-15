c9b2cf524f583321a50572be4ff5ffce4f0040d0
Alberto Casero <alberto@kasfactory.net>
2018-11-16 00:14:06 +0100

    Añadimos capacidad de responder y aceptar diferentes formatos

--- project/settings.py
+++ project/settings.py
@@ -163,5 +163,16 @@ LOGIN_URL = 'login'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 3
+    'PAGE_SIZE': 3,
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+        'rest_framework_xml.parsers.XMLParser',
+        'rest_framework_yaml.parsers.YAMLParser',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework_xml.renderers.XMLRenderer',
+        'rest_framework_yaml.renderers.YAMLRenderer',
+        'rest_framework_csv.renderers.CSVRenderer'
+    ),
 }
