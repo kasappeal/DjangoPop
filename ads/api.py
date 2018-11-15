2b6af003d2c47bbb8a45326cfe61981084b79975
Alberto Casero <alberto@kasfactory.net>
2018-11-15 21:19:40 +0100

    Reemplazamos los endpoints de detalle, actualización y borrado de un anuncio por una vista genérica

--- ads/api.py
+++ ads/api.py
@@ -1,7 +1,4 @@
-from rest_framework import status
-from rest_framework.generics import get_object_or_404, ListCreateAPIView
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from ads.models import Ad
 from ads.serializers import AdListSerializer, AdSerializer
@@ -15,21 +12,7 @@ class AdListAPIView(ListCreateAPIView):
         return AdListSerializer if self.request.method == 'GET' else AdSerializer
 
 
-class AdDetailAPIView(APIView):
+class AdDetailAPIView(RetrieveUpdateDestroyAPIView):
 
-    def get(self, request, pk):
-        ad = get_object_or_404(Ad, pk=pk)
-        serializer = AdSerializer(ad)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        ad = get_object_or_404(Ad, pk=pk)
-        serializer = AdSerializer(ad, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def delete(self, request, pk):
-        ad = get_object_or_404(Ad, pk=pk)
-        ad.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    queryset = Ad.objects.all()
+    serializer_class = AdSerializer
